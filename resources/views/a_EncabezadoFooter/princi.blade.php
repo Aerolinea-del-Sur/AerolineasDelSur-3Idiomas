8aa3f1e9be86805b87b1351685a02841eae30426
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-12-13 17:27:05 -0500

    Antigravity - SEO de todas las paginas correccion v2 - princi

--- resources/views/a_EncabezadoFooter/princi.blade.php
+++ resources/views/a_EncabezadoFooter/princi.blade.php
@@ -19,7 +19,7 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="description" content="{{ $seo['description'] }}" />
             <meta name="keywords" content="{{ $seo['keywords'] }}" />
-            <meta name="author" content="{{ $seo['author'] }}" />
+            <meta name="author" content="{{ $seo['author'] ?? 'Aerolínea del Sur' }}" />
             <link rel="canonical" href="{{ $seo['canonical'] }}" />
             
         <!-- Open Graph (Facebook, LinkedIn) -->
