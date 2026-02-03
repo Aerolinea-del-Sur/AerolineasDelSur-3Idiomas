bd2e9e934408a139895aadda33db72e4c452d778
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2026-02-03 16:38:44 -0500

    Actualización de imagenes del Manu + seo de imagenes v3

--- resources/views/e_Agencia/sobrevuelo-manu.blade.php
+++ resources/views/e_Agencia/sobrevuelo-manu.blade.php
@@ -128,8 +128,16 @@
     <!-- Sección Hero -->
     <section class="tour-hero">
         <div class="hero-background">
-            <img src="{{ asset('img/sobrevuelos/vista-aerea-boca-manu-confluencia-rios.webp') }}" alt="Parque Nacional del Manu desde el aire">
-            <div class="hero-overlay"></div>
+            <figure class="carousel-slide" itemscope itemtype="http://schema.org/ImageObject">
+                <img src="/img/sobrevuelos/vista-aerea-boca-manu-confluencia-rios.webp" 
+                    alt="Confluencia de los ríos Manu y Madre de Dios vista desde un sobrevuelo."
+                    title="Unión de aguas: Ríos Manu y Madre de Dios"
+                    loading="lazy" width="1200" height="600"
+                    itemprop="contentUrl">
+                <figcaption class="caption-overlay">
+                    <span itemprop="caption">Confluencia de los Ríos Manu y Madre de Dios</span>
+                </figcaption>
+            </figure>
         </div>
         <div class="hero-content">
             <div class="tour-badges">
