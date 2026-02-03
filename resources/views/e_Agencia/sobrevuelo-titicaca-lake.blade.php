bfbd3570568ec5cb1f1e3a8e9fcfcbaca36a6dc1
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2026-02-02 20:35:35 -0500

    Actualización de sobrevuelo de titicaca - quitar 1 de los recomendados

--- resources/views/e_Agencia/sobrevuelo-titicaca-lake.blade.php
+++ resources/views/e_Agencia/sobrevuelo-titicaca-lake.blade.php
@@ -375,24 +375,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="tour-card">
-                    <div class="tour-image">
-                        <img src="{{ asset('img/tours/tesoros-cusco-1.webp') }}" alt="Tesoros de Cusco">
-                        <div class="tour-badge"><?= __('tours.recomendados.tesoros_cusco_type') ?></div>
-                    </div>
-                    <div class="tour-content">
-                        <h4><?= __('tours.recomendados.tesoros_cusco_title') ?></h4>
-                        <p><?= __('tours.recomendados.tesoros_cusco_desc') ?></p>
-                        <div class="tour-details">
-                            <span><i class="fas fa-clock"></i> <?= __('tours.recomendados.tesoros_cusco_duration') ?></span>
-                            <span><i class="fas fa-users"></i> <?= __('tours.recomendados.tesoros_cusco_pers') ?></span>
-                        </div>
-                        <div class="tour-footer">
-                            <a href="/{{ app()->getLocale() }}/agencia/tesoros-cusco"
-                                class="tour-btn"><?= __('tours.recomendados.general') ?> >></a>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
     </section>
 
