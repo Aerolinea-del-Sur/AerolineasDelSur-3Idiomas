804cd7b8da6b7c17558240b119269ab90a42ec5e
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2026-02-02 20:36:47 -0500

    Actualización de sobrevuelo de titicaca - alineacion de incluido

--- resources/views/e_Agencia/sobrevuelo-titicaca-lake.blade.php
+++ resources/views/e_Agencia/sobrevuelo-titicaca-lake.blade.php
@@ -323,14 +323,15 @@
                         <li><i class="fas fa-times"></i> <?= __('tours.common.notincluded3') ?></li>
                     </ul>
                 </div>-->
+                <div class="important-notes">
+                    <h4><i class="fas fa-exclamation-triangle"></i><?= $h4_13 ?></h4>
+                    <ul>
+                        <li><?= __('tours.common.note1') ?></li>
+                        <li><?= __('tours.common.note3') ?></li>
+                    </ul>
+                </div>
             </div>
-            <div class="important-notes">
-                <h4><i class="fas fa-exclamation-triangle"></i><?= $h4_13 ?></h4>
-                <ul>
-                    <li><?= __('tours.common.note1') ?></li>
-                    <li><?= __('tours.common.note3') ?></li>
-                </ul>
-            </div>
+            
         </div>
     </section>
 
